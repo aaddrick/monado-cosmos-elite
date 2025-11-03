865ab61ab11af8598b7ba23327bf7cd05dec3f32
Jakob Bornecrantz <tbornecrantz@nvidia.com>
2025-11-03 22:32:21 +0000

    st/oxr: Sort extension list [NFC]
    Part-of: <https://gitlab.freedesktop.org/monado/monado/-/merge_requests/2612>

--- src/xrt/state_trackers/oxr/oxr_extension_support.h
+++ src/xrt/state_trackers/oxr/oxr_extension_support.h
@@ -797,17 +797,6 @@
 
 
 /*
- * XR_MNDX_oculus_remote
- */
-#if defined(XR_MNDX_oculus_remote) && defined(XRT_FEATURE_OPENXR_INTERACTION_MNDX)
-#define OXR_HAVE_MNDX_oculus_remote
-#define OXR_EXTENSION_SUPPORT_MNDX_oculus_remote(_) _(MNDX_oculus_remote, MNDX_OCULUS_REMOTE)
-#else
-#define OXR_EXTENSION_SUPPORT_MNDX_oculus_remote(_)
-#endif
-
-
-/*
  * XR_MNDX_egl_enable
  */
 #if defined(XR_MNDX_egl_enable) && defined(XR_USE_PLATFORM_EGL)
@@ -837,6 +826,17 @@
 #define OXR_EXTENSION_SUPPORT_MNDX_hydra(_) _(MNDX_hydra, MNDX_HYDRA)
 #else
 #define OXR_EXTENSION_SUPPORT_MNDX_hydra(_)
+#endif
+
+
+/*
+ * XR_MNDX_oculus_remote
+ */
+#if defined(XR_MNDX_oculus_remote) && defined(XRT_FEATURE_OPENXR_INTERACTION_MNDX)
+#define OXR_HAVE_MNDX_oculus_remote
+#define OXR_EXTENSION_SUPPORT_MNDX_oculus_remote(_) _(MNDX_oculus_remote, MNDX_OCULUS_REMOTE)
+#else
+#define OXR_EXTENSION_SUPPORT_MNDX_oculus_remote(_)
 #endif
 
 
@@ -964,10 +964,10 @@
     OXR_EXTENSION_SUPPORT_HTCX_vive_tracker_interaction(_) \
     OXR_EXTENSION_SUPPORT_MNDX_ball_on_a_stick_controller(_) \
     OXR_EXTENSION_SUPPORT_MNDX_blubur_s1(_) \
-    OXR_EXTENSION_SUPPORT_MNDX_oculus_remote(_) \
     OXR_EXTENSION_SUPPORT_MNDX_egl_enable(_) \
     OXR_EXTENSION_SUPPORT_MNDX_force_feedback_curl(_) \
     OXR_EXTENSION_SUPPORT_MNDX_hydra(_) \
+    OXR_EXTENSION_SUPPORT_MNDX_oculus_remote(_) \
     OXR_EXTENSION_SUPPORT_MNDX_psvr2_interaction(_) \
     OXR_EXTENSION_SUPPORT_MNDX_system_buttons(_) \
     OXR_EXTENSION_SUPPORT_MNDX_xdev_space(_)
